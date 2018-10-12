6eeaacc47ea54a6c026902cefe966101fa7adaae
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2018-10-12 06:10:16 +0530

    Create home button (Day 2 Part 1)

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, Text, TouchableHighlight } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 export default class App extends Component {
-  state = { greeting: "Welcome to React Native!" };
-  updateGreeting = () => {
-    this.setState({ greeting: "New Greeting!" });
+  onPress = () => {
+    alert("Button pressed");
   };
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>{this.state.greeting}</Text>
-        <Text onPress={this.updateGreeting}>Change greeting</Text>
+        {/* <View style={[styles.box, { backgroundColor: "red" }]} />
+        <View style={[styles.box, { backgroundColor: "green" }]} />
+    <View style={[styles.box, { backgroundColor: "blue" }]} /> */}
+        <TouchableHighlight
+          underlayColor="rgba(50,183,255,0.8)"
+          onPress={this.onPress}
+          style={styles.button}
+        >
+          <View style={styles.textContainer}>
+            <Icon name="home" size={20} color="white" />
+            <Text style={styles.text}>HOME</Text>
+          </View>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -18,14 +29,32 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, //this is the most parent container, so fill the entire screen
+    // don't have to write display:flex
+    // flexDirection: "row", //primary axis -> row (y-axis)
+    // justifyContent: "space-between", //layout items on primary axis
+    // alignItems: "flex-end" //layout items on secondary axis
+    justifyContent: "center"
+    //alignItems: "center"
+  },
+  // box: {
+  //   flex: 1, //fill up inside parent equally
+  //   margin: 10
+  // }
+  button: {
+    backgroundColor: "rgba(50,183,255,1)",
+    borderRadius: 30,
+    padding: 17,
+    margin: 10
+  },
+  textContainer: {
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    flexDirection: "row"
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
+  text: {
+    color: "white",
+    fontSize: 18,
+    marginLeft: 9
   }
 });
