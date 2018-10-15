5fb8b1bdf4723890d2fca5b671bb88e7fbc108f1
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2018-10-16 03:07:14 +0530

    Added form inputs (Day 3 Part 1)

--- App.js
+++ App.js
@@ -1,67 +1,63 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text, FlatList } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableHighlight,
+  TextInput,
+  Button
+} from "react-native";
 
 export default class App extends Component {
-  state = { people: [], refreshing: false, page: 1 };
-  componentDidMount() {
-    this.fetchData();
-  }
-  fetchData = async () => {
-    this.setState({ refreshing: true });
-    try {
-      const data = await fetch(
-        `https://swapi.co/api/people?page=${this.state.page}`
-      );
-      const json = await data.json(); //converts above promise to json
-      this.setState({
-        people: json.results,
-        page: this.state.page + 1,
-        refreshing: false
-      });
-    } catch (err) {
-      console.log("error fetching data");
-    }
+  state = { username: "", password: "", formData: "" };
+  onChangeText = (key, val) => {
+    this.setState({ [key]: val });
   };
-  renderItem = ({ item }) => {
-    return (
-      <View
-        style={{ padding: 15, borderBottomColor: "#ddd", borderBottomWidth: 1 }}
-      >
-        <Text style={{ fontSize: 20 }}>{item.name}</Text>
-        <Text style={{ color: "rgba(0,0,0,.4)" }}>Gender: {item.gender}</Text>
-      </View>
-    );
+  submit = () => {
+    const userData = {
+      username: this.state.username,
+      password: this.state.password,
+      signedIn: true
+    };
+    this.setState({ formData: JSON.stringify(userData) });
   };
   render() {
     return (
       <View style={styles.container}>
-        <Text>People:</Text>
-        <FlatList
-          onRefresh={this.fetchData}
-          refreshing={this.state.refreshing}
-          data={this.state.people}
-          keyExtractor={item => item.name}
-          renderItem={
-            this.renderItem //extracts keys for the list
-          }
+        <Text>Form demo</Text>
+        <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Username"
+          onChangeText={val => this.onChangeText("username", val)}
+          style={styles.input} //have to have style to be visible
         />
+        <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Password"
+          onChangeText={val => this.onChangeText("password", val)}
+          style={styles.input}
+          secureTextEntry={true}
+        />
+        <Button onPress={this.submit} title="Submit" />
+        <Text>{this.state.formData}</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    paddingVertical: 14,
-    flex: 1, //this is the most parent container, so fill the entire screen
-    // don't have to write display:flex
-    // flexDirection: "row", //primary axis -> row (y-axis)
-    // justifyContent: "space-between", //layout items on primary axis
-    // alignItems: "flex-end" //layout items on secondary axis
-    justifyContent: "center"
-    //alignItems: "center"
+  input: {
+    padding: 8,
+    margin: 10,
+    height: 50,
+    borderWidth: 1,
+    borderColor: "#666"
   },
-  text: {
-    fontSize: 22
+  container: {
+    paddingVertical: 30,
+    paddingHorizontal: 14,
+    flex: 1
   }
 });
