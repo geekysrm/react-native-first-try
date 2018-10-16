39abe0c776b9e3b493204fab12e7b0a3e4530a46
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2018-10-16 06:04:02 +0530

    Add CameraRoll to view photos (Day 3 Part 2)

--- App.js
+++ App.js
@@ -1,63 +1,88 @@
 import React, { Component } from "react";
 import {
+  Dimensions,
+  ScrollView,
   StyleSheet,
   View,
   Text,
-  TouchableHighlight,
-  TextInput,
-  Button
+  CameraRoll,
+  Button,
+  PermissionsAndroid,
+  Platform,
+  Image
 } from "react-native";
 
+const { width, height } = Dimensions.get("window");
+
 export default class App extends Component {
-  state = { username: "", password: "", formData: "" };
-  onChangeText = (key, val) => {
-    this.setState({ [key]: val });
+  state = { photos: [] };
+
+  checkPlatform = async () => {
+    if (Platform.OS === "ios") {
+      this.getPhotos();
+    }
+    if (Platform.OS === "android") {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+          {
+            title: "Alert",
+            message: "This app would like to access your camera roll."
+          }
+        );
+        console.log("granted: ", granted);
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          this.getPhotos();
+        } else {
+          console.log("Camera permission denied");
+        }
+      } catch (err) {
+        console.warn(err);
+      }
+    }
   };
-  submit = () => {
-    const userData = {
-      username: this.state.username,
-      password: this.state.password,
-      signedIn: true
-    };
-    this.setState({ formData: JSON.stringify(userData) });
+
+  getPhotos = async () => {
+    try {
+      const data = await CameraRoll.getPhotos({
+        first: 10,
+        assetType: "All"
+      });
+      console.log("data: ", data);
+      this.setState({ photos: data.edges });
+    } catch (err) {
+      console.log("error:", err);
+    }
   };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Form demo</Text>
-        <TextInput
-          autoCapitalize="none"
-          autoCorrect={false}
-          placeholder="Username"
-          onChangeText={val => this.onChangeText("username", val)}
-          style={styles.input} //have to have style to be visible
-        />
-        <TextInput
-          autoCapitalize="none"
-          autoCorrect={false}
-          placeholder="Password"
-          onChangeText={val => this.onChangeText("password", val)}
-          style={styles.input}
-          secureTextEntry={true}
-        />
-        <Button onPress={this.submit} title="Submit" />
-        <Text>{this.state.formData}</Text>
+        <Text>Camera Roll Demo</Text>
+        <Button onPress={this.checkPlatform} title="Get Photos" />
+        <ScrollView>
+          <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+            {this.state.photos.map((image, index) => (
+              <Image
+                source={{
+                  uri: image.node.image.uri
+                }}
+                style={{
+                  width: width / 2,
+                  height: width / 2
+                }}
+              />
+            ))}
+          </View>
+        </ScrollView>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  input: {
-    padding: 8,
-    margin: 10,
-    height: 50,
-    borderWidth: 1,
-    borderColor: "#666"
-  },
   container: {
     paddingVertical: 30,
-    paddingHorizontal: 14,
     flex: 1
   }
 });
